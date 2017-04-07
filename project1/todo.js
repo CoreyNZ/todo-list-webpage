f349494498d0c69311bcaf72e32adba29db2b494
CoreyNZ <CoreyWilkinsonNZ@gmail.com>
2017-04-07 15:25:30 +1200

    Almost have edit button functionality

--- project1/todo.js
+++ project1/todo.js
@@ -1,6 +1,7 @@
 // will only execute once the DOM is ready for Javascript code to execute. 
 $(document).ready(function(e) {
 	var selectedTask;
+	var editTask;
 	
 	
 	//the icon 'ui-icon-circle-plus' is provided by jQuery UI
@@ -12,13 +13,20 @@ $(document).ready(function(e) {
 		//highlights the space in the list where a user can drop an item
 		placeholder : 'ui-state-highlight',
 		//elements on the list that won't work as handles for dragging the item
-		cancel : '.delete,.done'	
+		cancel : '.delete,.done,.edit'	
 	});
 	
 	$('.sortlist').on('click','.delete',function(){
 		selectedTask = $(this);
 		$('#delete-dialog').dialog('open');
 	});
+	
+	
+	$('.sortlist').on('click','.edit',function(){
+		editTask = $(this);
+		$('#edit-dialog').dialog('open');
+	});
+	
 	
 	
 	$('#delete-dialog').dialog({
@@ -32,9 +40,6 @@ $(document).ready(function(e) {
 				});
 				
 				$(this).dialog('close');
-				
-				
-           	
 			},
 			
 			"No": function () { 
@@ -43,13 +48,42 @@ $(document).ready(function(e) {
 	});
 	
 	
-	$('#add-todo').button({icons: { primary: "ui-icon-circle-plus"}})
+	$('#edit-dialog').dialog({
+		modal: true, autoOpen: false,
+		buttons : {
+			"Yes": function () {
+				
+				//need to do something here.
+				selectedTask.parent('li').effect('pulsate',function() {
+					$(this).remove();
+				});
+				
+				$(this).dialog('close');
+			},
+			
+			"No": function () { 
+				$(this).dialog('close');}	
+			}
+	});
+	
+	
+	
+	
+	
+	
+	
+	
 	$('#add-todo').button({
 		icons: { primary: "ui-icon-circle-plus"}}).click(
 			function() {
 				$('#new-todo').dialog('open');
 				//$('#new-todo').dialog('close');
 			});
+		
+			
+	// ui-icon-plus -> jQuery icon for edit
+			
+			
 	$('#todo-list').on('click','.done',function() {
 		var $taskItem = $(this).parent('li');
 		$taskItem.slideUp(250, function() {
@@ -69,6 +103,7 @@ $(document).ready(function(e) {
 				if (taskName === '') { return false; }
 				var taskHTML = '<li><span class="done">%</span>';
 				taskHTML += '<span class="delete">x</span>';
+				taskHTML += '<span class="edit"></span>';
 				taskHTML += '<span class="task"></span></li>';
 				var $newTask = $(taskHTML);
 				$newTask.find('.task').text(taskName);
